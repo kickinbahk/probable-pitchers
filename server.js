edb4d74d26af36b6a59c0aa1f8982a093bdc00d3
kickinbahk <kickinbahk@gmail.com>
2016-04-02 20:37:18 -0700

    test received data

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 var express = require('express')
 var app = express()
 var http = require('http').Server(app)
+var appjs = require('./app.js')
+var Probables = require('mlbprobablepitchers')
 
 var PORT = 3000
 
 app.use(express.static(`${__dirname}/public`))
+
+// var data = appjs
+// console.log(data)
+
+var date = '2016/04/06' // 2016/04/06 (yyyy/mm/dd)
+var probablePitchers = []
+
+Probables.get(date, function (error, matchup) {
+  if (error) {
+    console.log('error')
+  } else {
+    console.log(matchup)
+  }
+})
 
 http.listen(PORT, function () {
   console.log(`Server listening on ${PORT}`)
