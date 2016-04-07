6dc7d3af71b80d1179f11a322fef5df9e43c6fb9
kickinbahk <kickinbahk@gmail.com>
2016-04-06 18:59:52 -0700

    add lodash

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
 var express = require('express')
 var app = express()
 var http = require('http').Server(app)
+var _ = require('lodash')
 var appjs = require('./app.js')
-var Probables = require('mlbprobablepitchers')
 
 var PORT = 3000
 
 app.use(express.static(`${__dirname}/public`))
 
-// var data = appjs
-// console.log(data)
-
-var date = '2016/04/08' // 2016/04/06 (yyyy/mm/dd)
-var probablePitchers = []
-
-Probables.get(date, function (error, matchup) {
-  if (error) {
-    console.log('error')
-  } else {
-    console.log(matchup)
-  }
-})
+var data = appjs
+console.log(data)
 
 http.listen(PORT, function () {
   console.log(`Server listening on ${PORT}`)
