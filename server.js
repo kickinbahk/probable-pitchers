352e742d87ff9afb86045f6105152ef52d772b63
kickinbahk <kickinbahk@gmail.com>
2016-04-08 19:28:42 -0700

    remove comments

--- server.js
+++ server.js
@@ -9,20 +9,6 @@ var PORT = 3000
 app.use(express.static(`${__dirname}/public`))
 
 appjs()
-// var gameData = appjs
-
-// gameData.then(function (data) {
-//   var allPitchers = []
-//   _.mapValues(data, function (p) {
-//     console.log(p.pitchers.away.name)
-//     allPitchers.push(p.pitchers)
-//   })
-//   console.log(allPitchers)
-//   return allPitchers
-// }).then(function (pitchers) {
-//   var pitchersGoing = _.mapValues(pitchers, 'name')
-//   console.log(pitchersGoing)
-// })
 
 http.listen(PORT, function () {
   console.log(`Server listening on ${PORT}`)
